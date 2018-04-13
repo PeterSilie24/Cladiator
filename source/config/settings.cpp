80e4703e85e8e407e51bf1351e00ad303002ebca
PeterSilie24 <progamer24de@gmail.com>
2018-04-13 13:58:12 +0200

    Fixed include problems and added the default translation files of Qt for English and German to the translations directory.

--- source/config/settings.cpp
+++ source/config/settings.cpp
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Settings.hpp"
+#include "settings.hpp"
 
 #include "dialogs/preferences.hpp"
 
--- source/dialogs/preferences.cpp
+++ source/dialogs/preferences.cpp
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Preferences.hpp"
+#include "preferences.hpp"
 
 Preferences::Preferences(const Settings::StructSettings& settings, QWidget* parent)
 	: QDialog(parent), settings(settings)
